d642be767cedcebfee54cf40458d2730083e8dae
Gripers <gripers91@gmail.com>
2023-02-04 14:40:32 +0500

    ef_cmt

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   fio: { type: String, required: true, unique: true },
-  password: { type: String },
   phone_number: { type: Number, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   tags: [{ type: String, unique: true }],
